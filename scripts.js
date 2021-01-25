6800bb33a9f97e4169a70f75ea0c33c430cdbe60
iammunir <otlicnoe.pismo@gmail.com>
2021-01-26 05:02:47 +0700

    refactoring and adding BEM

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 const cards = document.getElementsByClassName('card');
-const buyButtons = document.getElementsByClassName('btn-buy');
+const buyButtons = document.getElementsByClassName('card__button-buy');
 const modal = document.getElementById('modal-layer');
 const btnClose = document.getElementById('btn-close');
 
+// set event listener to card buttons
 for (let btn of buyButtons) {
     btn.addEventListener('click', (event) => {
         modal.style.display = 'block';
@@ -13,9 +14,10 @@ for (let btn of buyButtons) {
 // close the modal
 btnClose.addEventListener('click', () => modal.style.display = 'none');
 
+// set event listener to the cards
 for (let card of cards) {
     card.addEventListener('click', (event) => {
-        const overlay = card.getElementsByClassName('overlay')[0];
+        const overlay = card.getElementsByClassName('card__overlay')[0];
         overlay.style.opacity = 1;
         overlay.style.zIndex = 1;
         setTimeout(() => {
